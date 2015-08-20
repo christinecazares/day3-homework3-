6b88b352402d45ea7cdcbe9934b41024b1079114
Christine Cazares <3cccazares@gmail.com>
2015-08-20 09:12:18 -0500

    day 3 bomework 1

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rockpaperscissors
+{
+
+
+    interface IPlayer
+    {
+        string Act();
+        int NumbersofWins { get; set; }
+
+    }
+    class Rock : IPlayer
+    {
+        public int NumbersofWins
+        {
+            get;(Rock;< Scissors;)
+
+            set;
+        }
+
+        public string Act()
+        {
+            return "rock";
+        }
+    }
+    class Paper : IPlayer
+    {
+        public int NumbersofWins
+        {
+            get;
+
+            set;
+        }
+
+        public string Act()
+        {
+            return "paper";
+        }
+    }
+    class Scissors : IPlayer
+    {
+        public int NumbersofWins
+        {
+            get;
+
+            set;
+        }
+
+        public string Act()
+        {
+            return "scissors";
+        }
+    }
+    foreach (player1 , player2);{
+        if Rock<Scissors;
+                           return  "Rock";
+                           else; 
+                           if ;
+                           Scissors<Paper;
+                               return "scissors";
+                               else;
+                               if;
+                               Paper<Rock;
+                                   return Paper;
+        }
+Console.WriteLine("winer");
+    Console,ReadLine();                  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+
+
+
+
+
+         
